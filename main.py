22bf4aa75a70acaedcaaf8442f59adba12ce2705
Devparmar131213 <parmardev834@gmail.com>
2025-12-11 21:24:14 +0200

    Update greeting message on feature branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def greet(name: str) -> None:
     print(f"Hello, {name}! This is my Git assignment repo.")
+    print("Now with a feature branch change!")
 
 if __name__ == "__main__":
     greet("Teacher")
